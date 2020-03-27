b34e3bac5764ed3b96eeb471e8ffa02b26231575
Aryan <jakeryam123@gmail.com>
2020-03-27 21:18:07 +0530

    Build

---
+++ server/build/precache-manifest.0b7f3ecc029908e1021e107148a21e9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef5c13f847e28a1262ee75ed3f06de7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6414bff0bc8f8ba2c542",
+    "url": "/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "86a868f8af9d21af6f6e",
+    "url": "/static/js/2.ba27f52c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba27f52c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6414bff0bc8f8ba2c542",
+    "url": "/static/js/main.0726ad22.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
